3f63ee831ab47fbefc6ff015bfa946f1d983fe15
Lukas <lukas@thrane.name>
2023-10-06 09:53:53 +0200

    constants

--- finnanalytics/lib/constants.ts
+++ finnanalytics/lib/constants.ts
@@ -1,30 +1,22 @@
-export const MARKET_LIST = {
-    eiendom: 'realestate',
-    bil: 'bil',
-    torget: 'bap',
-    mc: 'mc',
-    båt: 'boat'
-};
+export const searchKeys = [
+    {label: 'Market', value: 'BAP_COMMON', vertical: 'bap'},
+    {label: 'Real Estate', value: 'REALESTATE_HOMES', vertical: 'realestate'},
+    {label: 'Car', value: 'CAR_USED', vertical: 'car'},
+    {label: 'MC', value: 'MC_USED', vertical: 'mc'},
+    {label: 'Boat', value: 'BOAT_USED', vertical: 'boat'},
+];
 
 export const Locations = [
-    {label: 'Akershus', value: '0.20003'},
-    {label: 'Aust-Agder', value: '0.20010'},
-    {label: 'Buskerud', value: '0.20007'},
-    {label: 'Finnmark', value: '0.20020'},
-    {label: 'Hedmark', value: '0.20005'},
-    {label: 'Hordaland', value: '0.20013'},
+    {label: 'Oslo', value: '0.20061'},
+    {label: 'Trøndelag', value: '0.20016'},
+    {label: 'Agder', value: '0.22042'},
+    {label: 'Innlandet', value: '0.22034'},
     {label: 'Møre og Romsdal', value: '0.20015'},
     {label: 'Nordland', value: '0.20018'},
-    {label: 'Oppland', value: '0.20006'},
-    {label: 'Oslo', value: '0.20061'},
     {label: 'Rogaland', value: '0.20012'},
-    {label: 'Sogn og Fjordane', value: '0.20014'},
-    {label: 'Telemark', value: '0.20009'},
-    {label: 'Troms', value: '0.20019'},
-    {label: 'Trøndelag', value: '0.20016'},
-    {label: 'Vestfold', value: '0.20008'},
-    {label: 'Vest-Agder', value: '0.20011'},
-    {label: 'Østfold', value: '0.20002'}
+    {label: 'Troms og Finnmark', value: '0.22054'},
+    {label: 'Vestfold og Telemark', value: '0.22038'},
+    {label: 'Vestland', value: '0.22046'},
 ];
 
 export const Sort = [
@@ -33,4 +25,4 @@ export const Sort = [
     {label: 'Old', value: '2'},
     {label: 'Price: Low - High', value: '3'},
     {label: 'Price: High - Low', value: '4'},
-]
+];
